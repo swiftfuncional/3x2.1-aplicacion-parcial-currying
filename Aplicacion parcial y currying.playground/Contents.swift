83523b31c82cf99e434f64c071e64e48245e84e2
Swift Funcional <info@swiftfuncional.com>
2017-03-14 14:29:29 +0100

    Apply currying

--- Aplicacion parcial y currying.playground/Contents.swift
+++ Aplicacion parcial y currying.playground/Contents.swift
@@ -1,5 +1,22 @@
 class Connector {
-	func connect(host: String = "localhost", port: Int = 80) {
+	func connect(host: String, port: Int) {
 		//Here, we have to do our conexion logic
+	}
+
+	func connect(host: String) {
+		connect(host: host, port: 80)
+	}
+
+	func connect() {
+		connect(host: "localhost")
+	}
+}
+
+func connect(host: String) -> (Int) -> ((Void) -> Void) {
+	return { port in
+		return {
+			//Here, we have to do our conexion logic
+		}
+
 	}
 }
